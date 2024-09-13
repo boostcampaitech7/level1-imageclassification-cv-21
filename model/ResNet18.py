7babc2247934fadb3dbd3fdea8d170d6e6af4fe4
Haneol-Kijm <haneol.kijm@gmail.com>
2024-09-13 11:11:04 +0900

    🎨 Add empty files and folders to make baseline structure(#4)

---
+++ model/ResNet18.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from torchvision import models
+import torch
+import torch.nn as nn
+
+class ResNet18(nn.Module):
+    """
+    ResNet18 model with optional additional layers or customizations.
+
+    Args:
+        num_classes (int, optional): Number of output classes. Defaults to None.
+        pretrained (bool, optional): Use pre-trained weights. Defaults to True.
+    """
+
+    def __init__(self, num_classes: Optional[int] = None, pretrained: bool = False):
+        super(ResNet18, self).__init__()
+
+        # Load pre-trained ResNet18 model
+        self.model = models.resnet18(pretrained=pretrained, progress=False)
+
+        # If num_classes is provided, replace the last layer
+        if num_classes is not None:
+            self.model.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass through the ResNet18 model.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output of the ResNet18 model.
+        """
+        return self.model(x)
