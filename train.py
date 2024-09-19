b9dcfa68adef87c2479e3d22a70bfdd507289550
Haneol-Kijm <haneol.kijm@gmail.com>
2024-09-20 00:14:43 +0900

    ✨ Add a feature to run test from checkpoint_path

--- train.py
+++ train.py
@@ -19,6 +19,11 @@ def tune_train_and_test(config):
         run_test(config, ckpt_dir)
 
 def main(config):
+    if config.experiment.checkpoint_path:
+        run_test(config, config.experiment.checkpoint_path)
+        return # Exit the program after test and saving the csv output
+    else:
+        pass
     if not torch.cuda.is_available():
         raise RuntimeError(f"CUDA not available. This program requires a CUDA-enabled NVIDIA GPU.")
 
@@ -32,14 +37,16 @@ if __name__ == "__main__":
     parser.add_argument('--num-gpus', type=int, help='Name of the model to use.')
     parser.add_argument('--smoke-test', action='store_true', help='Perform a small trial to test the setup.')
     parser.add_argument('--ddp', action='store_true', help='Perform the distributed data parallel. Only use when you have multiple gpus.')
+    parser.add_argument('--checkpoint-path', type=str, help='Path to the checkpoint to load and test.')
     args = parser.parse_args()
 
     # Initialize and configure the model configuration object
     ConfigClass = get_config(args.model_name)
     config = ConfigClass()
-    for key, value in vars(args).items():
-        if hasattr(config, key) and value is not None:
-            setattr(config, key)
+    config.update_from_args(args)
+    # for key, value in vars(args).items():
+    #     if hasattr(config, key) and value is not None:
+    #         setattr(config, key)
 
     # Perform smoke test if enabled
     if args.smoke_test:
