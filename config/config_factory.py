2b07ec85323d6d85df52a71935c4f787bc92e595
suhyun6363 <thebbbb1206@gmail.com>
2024-09-21 22:32:31 +0900

    ✨ Add ViT model

--- config/config_factory.py
+++ config/config_factory.py
@@ -1,12 +1,14 @@
 from .config import Config
 from .custom_nn_config import CustomNNConfig
-from .coatnet_config import CoAtNetConfig
+from .coatnet import CoAtNetConfig
+from .vit import ViTConfig
 
 # 모델 이름과 구성 설정 클래스를 매핑하는 디렉토리
 CONFIG_MAP = {
     'ResNet18': Config,
     'CustomNN': CustomNNConfig,
     'CoAtNet': CoAtNetConfig,
+    'ViT': ViTConfig
 }
 
 def get_config(model_name):
