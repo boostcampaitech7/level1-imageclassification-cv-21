f59b0ecc3da9ef8a227eb4dd98fb004f3d5e1520
Haneol-Kijm <haneol.kijm@gmail.com>
2024-09-22 18:15:17 +0900

    ✨ DeiT 모델 추가

--- config/config_factory.py
+++ config/config_factory.py
@@ -2,13 +2,15 @@ from .config import Config
 from .custom_nn_config import CustomNNConfig
 from .coatnet_config import CoAtNetConfig
 from .vit_config import ViTConfig
+from .deit3_config import DeiT3Config
 
 # 모델 이름과 구성 설정 클래스를 매핑하는 디렉토리
 CONFIG_MAP = {
     'ResNet18': Config,
     'CustomNN': CustomNNConfig,
     'CoAtNet': CoAtNetConfig,
-    'ViT': ViTConfig
+    'ViT': ViTConfig,
+    'DeiT3': DeiT3Config
 }
 
 def get_config(model_name):
---
+++ config/deit3_config.py
@@ -0,0 +1,16 @@
+from ray import tune
+
+from .config import Config
+
+
+class DeiT3Config(Config):
+    def __init__(self):
+        super().__init__()
+        self.model_name = "DeiT3"  # Override the baseline model name
+        self.training.batch_size = 64
+        self.training.lr = tune.loguniform(1e-6, 2e-4)
+        self.training.weight_decay = tune.loguniform(0.0001, 0.1)
+
+        self.experiment.num_workers = 3
+        self.experiment.num_samples = 20
+        self.experiment.max_epochs = 50
