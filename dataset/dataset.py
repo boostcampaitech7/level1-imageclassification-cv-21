d3f8021d5bb17a1764701f6d9704128cc441e6e0
kimmaru <comaoz1@naver.com>
2024-09-26 14:28:44 +0900

    🐛 fix the bug tta_transforms

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -30,7 +30,7 @@ class CustomDataset(Dataset):
             ]),
             transforms.Compose([
                 transforms.Resize((224, 224)), 
-                transforms.HorizontalFlip(p=1),
+                transforms.RandomHorizontalFlip(p=1),
                 transforms.ToTensor(), 
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ]),
